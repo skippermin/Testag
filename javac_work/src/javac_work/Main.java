e15695074012ab1a3f21d4d8f1f9fbcbd48b853f
jangminwoo <skipper44@naver.com>
2023-03-18 14:23:36 +0900

    1111

---
+++ javac_work/src/javac_work/Main.java
@@ -0,0 +1,10 @@
+package javac_work;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("안녕 애들아 잘 지내고 있니 이거 지금 임시 테스트 중");
+	}
+
+}
